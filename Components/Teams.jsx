26468b6089788a38957ae98050c9e200624c3c6e
newsunbanjade <thenewsunbanjade@gmail.com>
2021-02-20 09:58:19 +0545

    Blank Componets add yet to add content on it

---
+++ Components/Teams.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+SwiperCore.use([Navigation, Pagination, Autoplay]);
+const Teams = () => {
+  return <div></div>;
+};
+
+export default Teams;
